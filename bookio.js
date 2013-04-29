0d02fd6df26948033b84d0adae49700c381d1fee
Magnus Egelberg <magnus@egelberg.se>
2013-04-29 10:56:43 +0200

    Revert "-"
    This reverts commit 7d5cdab6fad09b0a61d61d41e7d5f27415ad7e18.

--- bookio.js
+++ bookio.js
@@ -1,11 +1,28 @@
 
-define(['jquery'], function($) {
+define(['jquery', 'scripts/gopher'], function($) {
 
 
 	var Module = function() {
 		
+		var _root = null;
+	    	
 		function init() {
+			//_root = $('bödy');
+			_root = $('body');
 		}
+		
+		this.root = function() {
+    		return _root;
+		}
+		
+		this.loadURL = function(url) {
+            $.get(url, function(html) {
+        		_root.empty();
+        		_root.append(html);
+            });  			
+		};
+		
+		this.gopher = new Gopher();
 		
 		// Initialize
 		init();

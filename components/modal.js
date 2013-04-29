5344f810636b6da993b6c0d822373ab9f60721db
Magnus Egelberg <magnus@egelberg.se>
2013-04-29 14:15:42 +0200

    Modal now uses Bootraps modal() call

--- components/modal.js
+++ components/modal.js
@@ -18,18 +18,12 @@ define(['jquery', 'less!components/modal', 'components/popover'], function($) {
 		
 		var _options = $.extend({}, _defaults, options);
 		var _modal = null;
-		var _backdrop = null; 
-
 		
 		function init() {
-			var backdropTemplate = 
-				'<div class="modal-backdrop hide">'+
-				'</div>';
-				
+
 			var bodyTemplate = 
-				'<div class="modal hide custom">'+
+				'<div class="modal hide custom fade">'+
 					'<div class="modal-header">'+
-						//'<label class="cancel">Avbryt</label>'+
 						'<button type="button" class="close" aria-hidden="true">&times;</button>'+
 					'</div>'+
 				'<div class="modal-body "></div>';
@@ -39,26 +33,26 @@ define(['jquery', 'less!components/modal', 'components/popover'], function($) {
 					'<button type="button" class="btn cancel-button">Cancel</button>'+
 				'</div>';
 			
-			var backdrop = $(backdropTemplate).appendTo($('body'));
 			var modal  = $(bodyTemplate).appendTo($('body'));
 			var header = modal.find('.modal-header');
 			var body   = modal.find('.modal-body');
-			//var closeButton = modal.find('.close');
 			
 			modal.css({margin: '0px', width:'auto', height:'auto'});
-			
-			
-			backdrop.css({opacity:0.15});
 			
 			if (_options.title) {
 				$('<label></label>').text(_options.title).appendTo(header);
 			}
-			if (_options.content)
+
+			if (_options.content) {
 				$(_options.content).appendTo(body);
+			}
 				
-			
 			modal.find('.close').click(function(){
 				_options.onClose();
+			});		
+
+			modal.find('hidden').click(function(){
+				modal.remove();
 			});		
 
         	var windowWidth = $(window).innerWidth();
@@ -69,29 +63,16 @@ define(['jquery', 'less!components/modal', 'components/popover'], function($) {
 			// Make some late modifications the position when we have the size
 			modal.css({left: Math.floor((windowWidth - modalWidth) / 2), top:Math.floor((windowHeight - modalHeight) / 3)});
 
-			
 			_modal = modal;
-			_backdrop = backdrop;
 		};
 		
 		function show() {
-			Popover.fadeOutPopovers();
-			
-			_backdrop.fadeIn(200);
-			_modal.fadeIn(200);
+		  _modal.modal('show');
 		}
 		
 		function close() {
-			_modal.fadeOut(100, function(){
-				_modal.remove();
-				_modal = null;
-			});
-
-			_backdrop.fadeOut(100, function(){
-				_backdrop.remove();
-				_backdrop = null;
-			});
-			
+		  _modal.modal('hide');
+		  _modal = null;
 		}
 		
 		init();

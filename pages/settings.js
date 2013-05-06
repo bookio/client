50deb99a0318ffa320f99ef2346a3d06000dd207
Magnus Egelberg <magnus@egelberg.se>
2013-05-06 14:56:20 +0200

    New ReuireJS

--- pages/settings.js
+++ pages/settings.js
@@ -15,7 +15,8 @@
 	
     	    var _elements = {};
     	    var _modal = null;
- 
+    	    var _gopher = new Gopher();
+    	    var _info = null;
 
 			var _defaults = {
 			};
@@ -34,31 +35,81 @@
 			
 			function enableClickSaveCompanyData() {
     	      
-    	        _elements.saveCompanyDataButton.click(function() {
-	    	        console.log("SaveCompanyButton");
+    	        _elements.saveButton.click(function() {
+                    chill();
+                            	        
+        	        var request = _gopher.request('PUT', 'settings/app/contact', _info);
+        	        
+        	        request.done(function(){
+            	        _modal.close();
+        	        });
     	        });
 	        };
 
-			
+            function fill() {
+	    	    if (_info.name)
+	    	        _elements.name.val(_info.name);
+
+	    	    if (_info.phone)
+	    	        _elements.phone.val(_info.phone);
+
+	    	    if (_info.email)
+	    	        _elements.email.val(_info.email);
+
+	    	    if (_info.address)
+	    	        _elements.address.val(_info.address);
+
+	    	    if (_info.webpage)
+	    	        _elements.webpage.val(_info.webpage);
+            }	        
+            
+            function chill() {
+                _info.name = _elements.name.val();
+                _info.phone = _elements.phone.val();
+                _info.email = _elements.email.val();
+                _info.address = _elements.address.val();
+                _info.webpage = _elements.webpage.val();
+            }
 	        
 	        function init() {
 	            
-	            _elements.html = $(html);
-	           
-	            _elements.saveCompanyDataButton = _elements.html.find("#settings-save-company-data-button");
-	    	    _elements.companyName = _elements.html.find("#inputCompanyName");
+    			var request = _gopher.request('GET', 'settings/app/contact');
+    			
+    			request.done(function(info) {
+                    _info = info ? info : {};
 
-	            _modal = new Modal({
-	                title: 'Inställningar',
-	                content:_elements.html
-	            });      
-	            
-	
-	            _modal.show();
-	      	    _elements.companyName.focus();
-
-	      	    enableEscKey();
-	            enableClickSaveCompanyData();
+    	            _elements.html = $(html);
+    	           
+    	            _elements.saveButton = _elements.html.find('.ok-button');
+    	    	    _elements.name = _elements.html.find('.name');
+    	    	    _elements.phone = _elements.html.find('.phone');
+    	    	    _elements.email = _elements.html.find('.email');
+    	    	    _elements.address = _elements.html.find('.address');
+    	    	    _elements.webpage = _elements.html.find('.webpage');
+        
+    	    	    _elements.html.find('.foo').on('click', function(){
+        	    	    var foo = _elements.html.find('.user-data');
+        	    	     
+        	    	    if (foo.is(":visible"))   
+            	    	    foo.slideUp('fast');
+            	    	  else
+            	    	    foo.slideDown('fast');
+    	    	    });
+    	    	    fill();
+    	    	    
+    	            _modal = new Modal({
+    	                title: 'Inställningar',
+    	                content:_elements.html
+    	            });      
+    	            
+    	
+    	            _modal.show();
+    	      	    _elements.name.focus();
+    
+    	      	    enableEscKey();
+    	            enableClickSaveCompanyData();
+        		});
+    
 	            
 	        }	  
 	        

951d7477b3701c26cbd700ad7bed296de1638ebd
Magnus Egelberg <magnus@egelberg.se>
2013-12-02 10:50:39 +0100

    -

--- widgets/symbolpicker/symbolpicker.js
+++ widgets/symbolpicker/symbolpicker.js
@@ -1,5 +1,3 @@
-
-
 (function() {
 
 	var dependencies = [
@@ -9,60 +7,64 @@
 
 	define(dependencies, function(html) {
 
-		
+
 		var Widget = function(widget) {
-		
+
 			var self = this;
-	
+
 			function init() {
-				
+
 				widget.element.append($(html));
 				widget.element.trigger('create');
-				
-	            widget.element.find('button').click(function() {
-	            	widget.element.find('.container').isotope({ filter: $(this).attr('data-filter') });   
+
+				widget.element.find('button').click(function() {
+					widget.element.find('.container').isotope({
+						filter: $(this).attr('data-filter')
+					});
 				});
 
-	            var isotope = widget.element.find('.container');
-	            
-	            // Fill container with symbols
-	            for (var i = 0; i < widget.options.symbols.length; i++) {
-	    			var icon = widget.options.symbols[i];
-		        	var div = $('<div class="symbol ' + icon.tags + '"></div>').appendTo(isotope);
-	                var image = sprintf('../../images/symbols/%s', icon.image);
-	    			var img = $('<img/>').attr('src', image).appendTo(div);
+				var isotope = widget.element.find('.container');
 
-	    			
-	    			img.data('symbol', icon);
+				// Fill container with symbols
+				for (var i = 0; i < widget.options.symbols.length; i++) {
+					var icon = widget.options.symbols[i];
+					var div = $('<div class="symbol ' + icon.tags + '"></div>').appendTo(isotope);
+					var image = sprintf('../../images/symbols/%s', icon.image);
+					var img = $('<img/>').attr('src', image).appendTo(div);
+
+
+					img.data('symbol', icon);
 
 					if (widget.options.selection && widget.options.selection.id == icon.id) {
-	    			     img.addClass('selected');
-					}	    			
+						img.addClass('selected');
+					}
 
-	    			img.on('tap', function(event) {
-	    			     isotope.find('img').removeClass('selected');
-	    			     $(this).addClass('selected');
-	    			     widget.element.trigger('symbolselected', $(this).data('symbol'));
-	    			});
-	
-	            }
-				            			
-				isotope.isotope({filter: '*'});
+					img.on('tap', function(event) {
+						isotope.find('img').removeClass('selected');
+						$(this).addClass('selected');
+						widget.element.trigger('symbolselected', $(this).data('symbol'));
+					});
+
+				}
+
+				isotope.isotope({
+					filter: '*'
+				});
 			}
 
-	        init();
-	    };
+			init();
+		};
 
 
 		// Define the widget
 		var widget = {};
-		
+
 		widget.options = {};
-		
+
 		widget._create = function() {
 			this.widget = new Widget(this);
 		}
-		
+
 		widget.symbols = function(value) {
 			this.widget.symbols(value);
 		}
@@ -81,7 +83,3 @@
 
 
 })();
-
-
-
-  

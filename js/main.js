7396bfeb232030d649d304d0445784571918ac8f
Joakim Bengtson <joakim@bookio.com>
2015-02-12 11:16:12 +0100

    bugfix

--- js/main.js
+++ js/main.js
@@ -174,12 +174,7 @@ requirejs.config({
 			var request = Gopher.request('GET', 'categories');
 	
 			request.done(function(categories) {
-				if (categories.length == 0) {
-					$.mobile.pages.go('pages/mobile/select-option.html');
-				}
-				else {
-					$.mobile.pages.go('pages/mobile/select-category.html');
-				}
+				$.mobile.pages.go('pages/mobile/select-category.html');
 			});
 
 		}

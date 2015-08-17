f5d08053d355d7e48d23adcf02d449084a344e0a
Joakim Bengtson <joakim@bookio.com>
2015-08-17 14:13:56 +0200

    -

--- pages/option/option.js
+++ pages/option/option.js
@@ -26,7 +26,8 @@
 				_elements.name.val(_option.name);
 				_elements.description.val(_option.description);
 				if (_option.image)
-					_elements.dropzone.imagepicker('setImage', _option.image);				
+					_elements.dropzone.imagepicker('setImage', _option.image);	
+				loadResources();			
 			}
 
 			function chill() {
@@ -35,8 +36,85 @@
 				_option.image = _elements.dropzone.imagepicker('getImage');
 			}
 			
-			function enableDisable() {				
-				((_elements.name.val().length == 0) ? _elements.save.addClass('ui-disabled') : _elements.save.removeClass('ui-disabled'));				
+			function enableDisable() {								
+				if (_elements.name.val().length == 0) {
+					_elements.save.addClass('ui-disabled');
+					_elements.rentals.addClass('ui-disabled');					
+				}
+				else {
+					_elements.save.removeClass('ui-disabled');
+					_elements.rentals.removeClass('ui-disabled');
+				}
+			}
+			
+			function loadResources() {
+				var request = Gopher.request('GET', 'rentals');
+
+				_elements.rentals.list('reset');
+				
+				request.done(function(rentals) {
+					
+					$.each(rentals, function(index, rental) {
+						var item = _elements.rentals.list('add', 'title subtitle icon-left');
+						item.title(rental.name);
+						item.subtitle(rental.description);
+
+						if ($.inArray(_option.id, rental.option_ids) != -1)
+							item.icon('check');
+							
+						item.element.on('tap', function() {
+							
+							if (_option.id == null) {
+								var request = saveOption();			
+										
+								request.done(function(option) {
+								});
+								
+							}
+
+							var index = rental.option_ids.indexOf(_option.id);
+							
+							if (index >= 0) {
+								rental.option_ids.splice(index, 1);
+								item.icon('');
+							}
+							else {
+								rental.option_ids.push(_option.id);
+								item.icon('check');
+							}
+							
+							$('body').spin('large');
+		
+							var request = Model.Rentals.save(rental);
+		
+							request.always(function() {
+								$('body').spin(false);
+							});							
+							
+	                    });
+						
+					});
+										
+					_elements.rentals.list('refresh');
+
+				});
+
+				return request;				
+			}
+			
+			function saveOption() {				
+				$.spin(true);
+				
+				chill();
+
+				var request = Model.Options.save(_option);
+
+				request.always(function() {
+					$.spin(false);
+				});
+				
+				return request;
+				
 			}
 
 			this.init = function() {
@@ -52,6 +130,30 @@
 
 				if (!_option.schedules)
 					_option.schedules = [];
+					
+				_elements.content.find('.tab-header [data-tab]').on('tap', function() {
+
+					var tab = $(this).attr('data-tab');
+
+					_elements.content.find('.tab-container').fadeOut(200, function() {								
+						$(this).find('[data-tab]').hide();
+						$(this).find(sprintf('[data-tab="%s"]', tab)).show();
+						
+						$(this).fadeIn(200);	
+					});
+					
+					// Set help text
+					if (_option.id == null)
+						_elements.help.text(i18n.text('help-resources-noname', 'The option (or service) must have a name.'));					
+					else
+						_elements.help.text(i18n.text('help-resources', 'Select resources that offers') + " '" + _elements.name.val() + "'");
+															
+				});
+											
+				_elements.content.find('.tab-container [data-tab]').hide();
+				_elements.content.find('.tab-container [data-tab]:first').show();
+				_elements.content.find('.tab-header [data-tab]:first').addClass('ui-btn-active');
+					
 					
 				_elements.back.on('tap', function(event) {
 					$.mobile.pages.pop(null);
@@ -88,21 +190,16 @@
 				});
 				
 				_element.on("pageshow", function(event) {
+					// Select the appropriate tab
+					var activeTab = _element.find('.tab-container [data-tab]:visible:first').attr('data-tab');
+					_element.find(sprintf('.tab-header [data-tab="%s"]', activeTab)).addClass('ui-btn-active');
+					
 					_elements.name.focus();
 				});								
 				
 				_elements.save.on('tap', function(event) {
-
-					$.spin(true);
-					
-					chill();
-
-					var request = Model.Options.save(_option);
-
-					request.always(function() {
-						$.spin(false);
-					});
-
+					var request = saveOption();			
+							
 					request.done(function(option) {
 						if ($.isFunction(page.params.callback)) {
 							page.params.callback(option);
@@ -112,8 +209,8 @@
 
 				});
 
-				fill();
-				
+				fill();				
+								
 				enableDisable();
 
 			}
